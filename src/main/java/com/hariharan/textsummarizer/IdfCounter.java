2a6b259d04f90e11c23cdc8cc488322b44c0cc62
Hariharan <theroboticlabs@gmail.com>
2017-11-14 18:25:03 +0530

    Computes similarity between 2 sentences.

--- src/main/java/com/hariharan/textsummarizer/IdfCounter.java
+++ src/main/java/com/hariharan/textsummarizer/IdfCounter.java
@@ -12,23 +12,32 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.lang.*;
 /**
  *
  * @author thero
  */
 public class IdfCounter {
     
-    private String text;
+    public String text; //Holds the normalize text
     private String[] allWords;
-    private HashMap<String, Integer> wordMap;
+    private HashMap<String, Integer> wordMap; //Stores the frequency of each word
+    private HashMap<String, Integer> sentences; //Holds all the sentences
     public void setText(String text)
     {
         this.text = normalize(text);
+        this.sentences = new HashMap<>();
+        for(String s: text.split("\\.")) //Escape for literal dot.
+        {
+            //System.out.println(s);
+            this.sentences.put(normalize(s),1); //Initialize the score of each sentence as 1
+        }
         this.allWords = this.text.split(" ");
         this.wordMap = count(allWords);
         
@@ -36,7 +45,7 @@ public class IdfCounter {
     
     public String normalize(String text)
     {
-        String normalized = text.replaceAll("[^a-zA-Z ]", " ").replaceAll(" +", " ").toLowerCase();
+        String normalized = text.replaceAll("[^a-zA-Z]", " ").replaceAll(" +", " ").toLowerCase();
         return normalized;
     }
     
@@ -68,38 +77,46 @@ public class IdfCounter {
             System.out.println(key+":"+wordMap.get(key));
         }
     }
+    
+    public double computeCosine(String input)
+    {
+        //Finds the cosine between the input and all the other sentences in the document
+        HashSet<String> inputVector = (HashSet)vectorize(input);
+        HashSet<String> sentenceVector = null;
+        Set<String> allSentences = this.sentences.keySet();
+        double cosineSum = 0; //Sum of cosines of the input with all sentences.
+        for(String eachSentence: allSentences)
+        {
+            //System.out.println(eachSentence);
+            double cosine = 0;
+            int count = 0;
+            sentenceVector = (HashSet)vectorize(eachSentence);
+            count = sentenceVector.size() + inputVector.size();
+            sentenceVector.retainAll(inputVector);
+            for(String t: sentenceVector)
+            {
+                //System.out.println(t);
+                int freq = wordMap.get(t);
+                cosine += Math.log10(count/freq);
+            }
+            cosineSum += cosine;
+        }
+        return cosineSum;
+    }
+    
+    public Set<String> vectorize(String text)
+    {
+        Set<String> vector = new HashSet<>();
+        for(String s: text.split(" "))
+        {
+            vector.add(s);
+        }
+        return vector;
+    }
     public static void main(String[] args) throws IOException
     {
        IdfCounter counter = new IdfCounter();
-       //counter.showFreq();
-       File dir = new File("data/business");
-       BufferedReader r;
-       FileReader fr;
-       String t;
-       String s = "";
-       File[] files = dir.listFiles();
-       for(File f: files)
-       {
-    
-           
-           try
-           {
-               fr = new FileReader(f.getAbsolutePath());
-               System.out.println(f.getAbsolutePath());
-               r = new BufferedReader(fr);
-               while((t = r.readLine()) != null)
-               {
-                   s += t;
-               }
-           }
-           catch(FileNotFoundException e)
-           {
-               System.out.println(e.getMessage());
-           }
-           
-           
-       }
-       counter.setText(s);
-       counter.showFreq();
+       counter.setText("This is a foo bar sentence. This sentence is similar to a foo bar sentence.");
+       System.out.println(counter.computeCosine(""));
     }
 }
