f59799120e18b310eb0ac9a2cb8dca3ce5f178ea
JAIVRAT SAROHA <jaivratsaroha27@gmail.com>
2017-11-29 19:11:32 +0530

    Login page

---
+++ Login/src/main/java/com/LoginController.java
@@ -0,0 +1,20 @@
+//import com.gluonhq.charm.glisten.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Button signupBtn;
+
+}
