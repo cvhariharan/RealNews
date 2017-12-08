33065778d502138a9cb1072e8d4387b860c81c1d
boomeraang <sooryankarthick@gmail.com>
2017-12-08 19:19:53 +0530

    added ID,likes,fakes to class news

--- src/main/java/com/soorya/webscrapers/News.java
+++ src/main/java/com/soorya/webscrapers/News.java
@@ -8,6 +8,7 @@ public class News
     String content;
     String tags;
     String url;
+    int ID,likes,fakes;
 
     News(String timestamp, String title, String author, String content, String tags, String url)
     {
@@ -17,6 +18,9 @@ public class News
         this.content = content;
         this.tags = tags;
         this.url = url;
+        this.likes = 0;
+        this.fakes = 0;
+        this.ID = title.hashCode();
     }
 
     News(String timestamp,String title,String author,String content,String url)
@@ -26,5 +30,28 @@ public class News
         this.authorName = author;
         this.content = content;
         this.url = url;
+        this.likes = 0;
+        this.fakes = 0;
+        this.ID = title.hashCode();
+    }
+
+    public int getLikes()
+    {
+        return likes;
+    }
+
+    public void incrementLikes()
+    {
+        this.likes++;
+    }
+
+    public int getFakes()
+    {
+        return fakes;
+    }
+
+    public void incrementFakes()
+    {
+        this.fakes++;
     }
 }
