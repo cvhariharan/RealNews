787ac5cba44e6cf726ddfa883bea9073f89ec7b7
Hariharan <theroboticlabs@gmail.com>
2017-12-08 19:40:46 +0530

    Initial main project GUI

---
+++ src/main/java/com/brocode/mainproject/MainPageController.java
@@ -0,0 +1,28 @@
+package com.brocode.mainproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.RadioButton;
+
+public class MainPageController {
+
+    @FXML
+    private Button updatePasswordBtn;
+
+    @FXML
+    private RadioButton worldCategoryBtn;
+
+    @FXML
+    private RadioButton sciCategoryBtn;
+
+    @FXML
+    private RadioButton sportsCategoryBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Pagination pageBtn;
+
+}
